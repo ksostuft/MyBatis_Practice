b9e065540eb97b828d25b40bb69224ad01af1502
ksostuft <ksostuft@gmail.com>
2023-08-09 18:33:20 +0900

    ivz tos

--- src/main/java/com/management/product/controller/ProductController.java
+++ src/main/java/com/management/product/controller/ProductController.java
@@ -45,11 +45,7 @@ public class ProductController {
         //    (조건 3) 제품 목록이 없는 경우, Print 객체를 통해 조회 결과가 없다는 오류 메세지를 출력하세요.
         List<ProductDTO> productList = productService.selectProductByCondition(searchCondition);
 
-        if(searchCondition.getValue() != null && "".equals(searchCondition.getValue())) {
-            System.out.println("1234");
-        }
-
-        if(productList != null && productList.size() > 0) {
+        if(productList != null && productList.size() > 0 && searchCondition.getValue().length() != 0) {
             productPrint.printProductList(productList, searchCondition);
         } else {
             productPrint.printErrorMessage("selectOne");
